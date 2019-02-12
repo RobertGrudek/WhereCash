9d179736f7f18866d1fbd4e545ce6bd2e8372b8e
rober <rober@DESKTOP-FE9EMCO>
2019-02-12 20:33:59 +0100

    Add project files.

---
+++ WhereCash/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WhereCash.Models;
+
+namespace WhereCash.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.CurrentDate = DateTime.Now.ToString("dddd, dd MMMM yyyy");
+            return View("MyView");
+        }
+    }
+}
